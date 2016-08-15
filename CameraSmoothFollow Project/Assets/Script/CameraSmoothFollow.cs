342131a7feb529a32b09262187dea1951e784195
lPinchol <pinchodev91@gmail.com>
2016-08-15 23:20:36 +0200

    Update 1.0.0

---
+++ CameraSmoothFollow Project/Assets/Script/CameraSmoothFollow.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+  
+public class CameraSmoothFollow : MonoBehaviour {
+
+	public GameObject target;
+	public float MaxLeftPos;
+	public float MaxRightPos;
+	public Vector3 targetPos;
+
+	private float delta;
+    private Vector3 offset = new Vector3(0,.13f,0);
+
+     void Update() {
+         if (target) {
+
+             Vector3 pos = transform.position;
+             pos.z = target.transform.position.z;
+
+             //target follow
+             Vector3 targetDirection = (target.transform.position - pos);
+             delta = targetDirection.magnitude * 5f;
+             targetPos = transform.position + (targetDirection.normalized * delta * Time.deltaTime); 
+
+             //clamp values
+             targetPos = new Vector3(Mathf.Clamp(targetPos.x, MaxLeftPos, MaxRightPos), targetPos.y, targetPos.z);
+
+             //move camera
+             transform.position = Vector3.Lerp( transform.position, targetPos + offset, .3f);
+         }
+     }
+ }
